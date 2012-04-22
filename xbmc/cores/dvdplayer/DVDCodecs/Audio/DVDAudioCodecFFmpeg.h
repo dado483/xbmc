ed745af431574007281291cc6faff3017784cf9c
Geoffrey McRae <gnif@xbmc.org>
2012-04-22 19:16:48 +1000

    [DVDP] added ifdefs for TARGET_DARWIN for apple specific LPCM mode

--- xbmc/cores/dvdplayer/DVDCodecs/Audio/DVDAudioCodecFFmpeg.h
+++ xbmc/cores/dvdplayer/DVDCodecs/Audio/DVDAudioCodecFFmpeg.h
@@ -39,6 +39,7 @@ public:
   virtual int GetChannels();
   virtual CAEChannelInfo GetChannelMap();
   virtual int GetSampleRate();
+  virtual int GetEncodedSampleRate();
   virtual enum AEDataFormat GetDataFormat();
   virtual const char* GetName() { return "FFmpeg"; }
   virtual int GetBufferSize() { return m_iBuffered; }
